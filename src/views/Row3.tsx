65ac7d487179bdee14f11881a862150966e67f54
Ignacio <55958716+kiltro-dev@users.noreply.github.com>
2023-04-17 09:30:34 -0400

    transaction done

--- src/views/Row3.tsx
+++ src/views/Row3.tsx
@@ -1,20 +1,44 @@
 import BoxHeader from '@/components/BoxHeader';
 import DashboardCard from '@/components/DashboardCard';
+import FlexBetween from '@/components/FlexBetween';
 import ResponsiveContainerWrapper from '@/components/ResponsiveContainerWrapper';
 import {
   useGetKpisQuery,
   useGetProductsQuery,
   useGetTransactionsQuery,
 } from '@/state/api';
-import { Box, useTheme } from '@mui/material';
+import { Box, Typography, useTheme } from '@mui/material';
 import { DataGrid, GridCellParams } from '@mui/x-data-grid';
+import { useMemo } from 'react';
+import { Cell, Pie, PieChart } from 'recharts';
 
 const Row3 = () => {
   const { palette } = useTheme();
+  const pieColors = [palette.primary[800], palette.primary[500]];
+
   const { data: kpiData } = useGetKpisQuery();
   const { data: productData } = useGetProductsQuery();
   const { data: transactionData } = useGetTransactionsQuery();
-  console.log(transactionData);
+
+  const pieChartData = useMemo(() => {
+    if (kpiData) {
+      const totalExpenses = kpiData[0].totalExpenses;
+      return Object.entries(kpiData[0].expensesByCategory).map(
+        ([key, value]) => {
+          return [
+            {
+              name: key,
+              value: value,
+            },
+            {
+              name: `${key} of Total`,
+              value: totalExpenses - value,
+            },
+          ];
+        },
+      );
+    }
+  }, [kpiData]);
 
   const dataGridStyle = {
     '& .MuiDataGrid-root': {
@@ -73,7 +97,8 @@ const Row3 = () => {
       field: 'productIds',
       headerName: 'Count',
       flex: 0.1,
-      renderCell: (params: GridCellParams) => params.value.length,
+      renderCell: (params: GridCellParams) =>
+        (params.value as Array<string>).length,
     },
   ];
 
@@ -86,12 +111,7 @@ const Row3 = () => {
           sideText={`${productData?.length} products`}
         />
         <Box mt="0.5rem" p="0 0.5rem" height="75%" sx={dataGridStyle}>
-          <ResponsiveContainerWrapper
-            mt="0.5rem"
-            p="0 0.5rem"
-            height="75%"
-            sx={dataGridStyle}
-          >
+          <ResponsiveContainerWrapper>
             <DataGrid
               columnHeaderHeight={25}
               rowHeight={35}
@@ -119,8 +139,55 @@ const Row3 = () => {
           </ResponsiveContainerWrapper>
         </Box>
       </DashboardCard>
-      <DashboardCard gridArea={'i'}></DashboardCard>
-      <DashboardCard gridArea={'j'}></DashboardCard>
+      <DashboardCard gridArea={'i'}>
+        <BoxHeader title="Expense Breakdown By Category" sideText="+4%" />
+        <FlexBetween mt="0.5rem" gap="0.5rem" p="0 1rem" textAlign="center">
+          {pieChartData?.map((data, i) => (
+            <Box key={`${data[0].name}-${i}`}>
+              <PieChart width={110} height={100}>
+                <Pie
+                  stroke="none"
+                  data={data}
+                  innerRadius={18}
+                  outerRadius={35}
+                  paddingAngle={2}
+                  dataKey="value"
+                >
+                  {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={pieColors[index]} />
+                  ))}
+                </Pie>
+              </PieChart>
+              <Typography variant="h5">{data[0].name}</Typography>
+            </Box>
+          ))}
+        </FlexBetween>
+      </DashboardCard>
+      <DashboardCard gridArea={'j'}>
+        <BoxHeader
+          title="Overall Summary and Explanation Data"
+          sideText="+15%"
+        />
+        <Box
+          height="15px"
+          margin="1.25rem 1rem 0.4rem 1rem"
+          bgcolor={palette.primary[800]}
+          borderRadius="1rem"
+        >
+          <Box
+            height="15px"
+            bgcolor={palette.primary[600]}
+            borderRadius="1rem"
+            width="40%"
+          ></Box>
+        </Box>
+        <Typography margin="0 1rem" variant="h6">
+          Orci aliquam enim vel diam. Venenatis euismod id donec mus lorem etiam
+          ullamcorper odio sed. Ipsum non sed gravida etiam urna egestas
+          molestie volutpat et. Malesuada quis pretium aliquet lacinia ornare
+          sed. In volutpat nullam at est id cum pulvinar nunc.
+        </Typography>
+      </DashboardCard>
     </>
   );
 };
